7b94c35c1ae2c4d3726ffd337634069e1a7dca44
Chetan <chetankhulage350@gmail.com>
2022-08-07 21:39:18 +0530

    two functions added (dummy text and clear)

--- src/components/TextModifyForm.js
+++ src/components/TextModifyForm.js
@@ -15,18 +15,27 @@ export default function TextModifyFrom() {
         let newText = text.toLowerCase();
         setText(newText);
     }
+    function handleClearButton() {
+        setText('');
+    }
+    function handleAutoTextButton() {
+        setText(text + 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. In dolore temporibus est debitis? Voluptate recusandae architecto consequatur sed itaque ratione reiciendis officia labore quas dolore accusamus voluptas ad, praesentium suscipit.');
+    }
 
-    const [text, setText] = useState('enter your text here');
+    const [text, setText] = useState('');
     return (
-        <div className='form flex' action="">
+        <div className='form flex'>
             <h1>Your text :</h1>
-            <textarea name="text" value={text} onChange={handleOnChange} placeholder='Enter Text'></textarea>
+            <textarea name="text" value={text} onChange={handleOnChange} placeholder='Enter Text Here'></textarea>
             <div className="buttons flex">
+
+                <button onClick={handleAutoTextButton}>create dummy text</button>
                 <button onClick={handleUpperButton}>Convert to Uppercase</button>
                 <button onClick={handleLowerButton}>Convert to Lowercase</button>
+                <button onClick={handleClearButton}>Clear All</button>
             </div>
             <div className="summary">
-                <h1 className="title">Your content summary</h1>
+                <h1 className="title">Your content summary :</h1>
                 <ul>
                     <li>
                         No. of characters: {text.length}
